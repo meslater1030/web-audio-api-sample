d7868a496c3fc614b7c69b5e0e02263241265c1e
Slater <megan.e.slater@gmail.com>
2018-02-19 18:33:45 -0800

    Step Two: Change the volume using the audio context.

--- src/App.js
+++ src/App.js
@@ -6,31 +6,50 @@ class App extends Component {
     super(props);
     this.state = {
       currentTime: 0,
+      paused: true,
+      muted: false,
+      volume: '100',
     };
+
+    // Check this out!
+    this.audioContext = new AudioContext();
   }
 
-  play = () => {
-    // Implement me
+  playPause = () => {
+    this.setState({ paused: !this.state.paused });
+    if (this.state.paused) {
+      this.videoElement.play();
+    } else {
+      this.videoElement.pause();
+    }
   }
 
-  pause = () => {
-    // Implement me
-  }
-
-  mute = () => {
-    // Implement me
-  }
-
-  unmute = () => {
-    // Implement me
+  muteUnmute = () => {
+    this.setState({ muted: !this.state.muted });
+    this.videoElement.muted = !this.videoElement.muted;
   }
 
   handleTimeChange = (e) => {
     this.setState({ currentTime: e.target.value });
+    if (this.videoDuration && this.videoDuration !== Infinity) {
+      this.videoElement.currentTime = this.videoDuration * Number(e.target.value) / 100;
+    }
   }
 
   handleDurationChange = (e) => {
     // What might this be useful for?
+    this.videoDuration = this.videoElement.duration;
+  }
+
+  // Check this out!
+  setupVideoElement = (videoElement) => {
+    this.videoElement = videoElement;
+    this.sourceNode = this.audioContext.createMediaElementSource(videoElement);
+    // Implement the remainder of this
+  }
+
+  handleVolumeChange = (e) => {
+    // Implement me
   }
 
   render() {
@@ -38,27 +57,22 @@ class App extends Component {
       <div>
         <video
           crossOrigin="anonymous"
-          ref={(videoElement) => { this.videoElement = videoElement; }}
-          playsInline="false"
+          ref={this.setupVideoElement}
           onDurationChange={this.handleDurationChange}
         >
           <source src="https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c0/Big_Buck_Bunny_4K.webm/Big_Buck_Bunny_4K.webm.360p.webm" type="video/webm" crossOrigin="anonymous" />
         </video>
         <div>
-          <button onClick={this.play}>
-            Play
+          <button onClick={this.playPause}>
+            {this.state.paused ? 'Play' : 'Pause'}
           </button>
-          <button onClick={this.pause}>
-            Pause
-          </button>
-          <button onClick={this.mute}>
-            Mute
-          </button>
-          <button onClick={this.unmute}>
-            Unmute
+          <button onClick={this.muteUnmute}>
+            {this.state.muted ? 'Unmute' : 'Mute'}
           </button>
           <label htmlFor="time">Current Time</label>
-          <input type="range" value={this.state.currentTime} onChange={this.handleTimeChange} />
+          <input id="time" type="range" value={this.state.currentTime} onChange={this.handleTimeChange} />
+          <label htmlFor="volume">Volume</label>
+          <input id="volume" type="range" value={this.state.volume} onChange={this.handleVolumeChange} />
         </div>
       </div>
     );
