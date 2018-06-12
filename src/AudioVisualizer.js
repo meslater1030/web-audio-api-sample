cf9c8009746e333a910f3bc5fd29b195a86f6fe8
Slater <megan.e.slater@gmail.com>
2018-06-12 10:58:24 -0700

    Add Sine Wave to audio visualizer

--- src/AudioVisualizer.js
+++ src/AudioVisualizer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from '@material-ui/lab/Slider';
 import Typography from '@material-ui/core/Typography';
 import Bar from './Bar';
+import { Checkbox, FormControlLabel } from '@material-ui/core';
 
 export default class AudioVisualizer extends React.Component {
   constructor(props) {
@@ -11,10 +12,59 @@ export default class AudioVisualizer extends React.Component {
       smoothingTimeConstant: 30,
       visualizerBars: {},
       barWidth: 14,
+      showSineWave: false,
     };
     props.analyserNode.fftSize = 64;
     props.analyserNode.smoothingTimeConstant = 0.3;
     this.setupVisualizerInterval();
+  }
+
+  setupSinewave = () => {
+    const canvas = this.canvasElement;
+    const canvasCtx = canvas.getContext("2d");
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+    const bufferLength = this.props.analyserNode.fftSize;
+    const dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    const draw = () => {
+
+      requestAnimationFrame(draw);
+
+      this.props.analyserNode.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = 'white';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'hsl(153, 90%, 59%)';
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = WIDTH * 1.0 / bufferLength;
+      let x = 0;
+
+      for(let i = 0; i < bufferLength; i++) {
+
+        const v = dataArray[i] / 128.0;
+        const y = v * HEIGHT/2;
+
+        if(i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height/2);
+      canvasCtx.stroke();
+    };
+
+    draw();
   }
 
   setupVisualizerInterval = () => {
@@ -24,7 +74,7 @@ export default class AudioVisualizer extends React.Component {
     clearInterval(this.interval);
 
     this.interval = setInterval(() => {
-      // Fill the array with information about the volume at various frequencies
+      // Fill the array with information about the volume at constious frequencies
       this.props.analyserNode.getFloatFrequencyData(dataArray);
       const visualizerBars = {}
       for (let i = 0; i < dataArray.length; i++) {
@@ -66,6 +116,10 @@ export default class AudioVisualizer extends React.Component {
     return this.setupVisualizerInterval();
   }
 
+  toggleVisualization = (e, value) => {
+    this.setState({ showSineWave: value });
+  }
+
   render() {
     const bars = Object.keys(this.state.visualizerBars)
       .map(bar => <Bar key={bar} height={this.state.visualizerBars[bar]} width={this.state.barWidth} />);
@@ -75,9 +129,22 @@ export default class AudioVisualizer extends React.Component {
         <Slider value={this.state.smoothingTimeConstant} aria-labelledby="smoothing-time-constant" onChange={this.handleSmoothingTimeConstantChange} />
         <Typography id="fft-size">FFT Size</Typography>
         <Slider aria-labelledby="fft-size" value={this.state.fftSize} onChange={this.handleFftSizeChange} />
-        <div style={{ display: 'flex', alignItems: 'flex-end', alignContent: 'flex-end', height: '200px', width: '500px' }}>
-          {bars}
-        </div>
+        <FormControlLabel control={ <Checkbox value={this.state.showSineWave} onChange={this.toggleVisualization} />} label="Show Sine Wave" />
+        {this.state.showSineWave && <canvas
+          ref={(canvasElement) => {
+            if (!this.canvasElement) {
+              this.canvasElement = canvasElement;
+              this.setupSinewave()
+            }
+          }}
+          width="500"
+          height="200"
+        />}
+        {!this.state.showSineWave &&
+          <div style={{ display: 'flex', alignItems: 'flex-end', alignContent: 'flex-end', height: '200px', width: '500px' }}>
+            {bars}
+          </div>
+        }
       </div>
     );
   }
