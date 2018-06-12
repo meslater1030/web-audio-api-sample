46d316ad8e6db9506044745a9655041b9ab8bfc1
Slater <megan.e.slater@gmail.com>
2018-06-11 21:23:32 -0700

    Add Panning code example

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import RowWrapper from './components/RowWrapper';
 import VisualizerCode from './components/VisualizerCode';
 import VideoCodeExample from './components/VideoCodeExample';
 import ConvolverCodeExample from './components/ConvolverCodeExample';
+import PanningCodeExample from './components/PanningCodeExample';
 
 class App extends Component {
   constructor(props) {
@@ -199,6 +200,7 @@ class App extends Component {
                   <div style={{ width: '500px' }}>
                     <Typeography id="panner">Panner</Typeography>
                     <Slider aria-labelledby="panner" value={this.state.pan} onChange={this.handlePanningChange} />
+                    <PanningCodeExample />
                   </div>
                 }
                 oscillationControls={<Keyboard playNote={this.playNote} stopPlaying={this.stopPlaying} />}
---
+++ src/components/PanningCodeExample.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import FullPageDialog from './FullPageDialog';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { light } from 'react-syntax-highlighter/styles/prism';
+import AudioNode from './AudioNode';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import Typography from '@material-ui/core/Typography';
+
+export default function PanningCodeExample() {
+    const codeString = `
+        // Part One: Set up the Audio Graph
+        cont videoElement = document.querySelector('video');
+        const audioContext = new AudioContext();
+        const sourceNode = audioContext.createMediaElementSource(videoElement);
+        const gainNode = qudioContext.createGain();
+        const pannerNode = audioContext.createStereoPanner();
+        sourceNode.connect(pannerNode);
+        pannerNode.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+
+        // Part Two: Pan the audio
+        pannerNode.pan.value = 0.20 // This value is between 0 and 1
+
+    `;
+    return <FullPageDialog dialogTitle="Convolver Code Example" dialogContent={ 
+        <React.Fragment>
+            <SyntaxHighlighter language='javascript' style={light}>{codeString}</SyntaxHighlighter>
+            <Typography variant="title">
+                Audio Graph
+            </Typography>
+            <AudioNode>
+                <Typography color="inherit" variant="body2">Source: Video Element</Typography>
+            </AudioNode>
+            <ArrowForwardIcon />
+            <AudioNode>
+                <Typography color="inherit" variant="body2">Effect: Panning Node</Typography>
+            </AudioNode>
+            <ArrowForwardIcon />
+            <AudioNode>
+                <Typography color="inherit" variant="body2">Effect: Gain Node (Volume)</Typography>
+            </AudioNode>
+            <ArrowForwardIcon />
+            <AudioNode>
+                <Typography color="inherit" variant="body2">Destination: Default (computer speakers)</Typography>
+            </AudioNode>
+        </React.Fragment>
+    }/>
+}
